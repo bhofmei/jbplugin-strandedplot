6e08f99685507ded018fe0256a7f05d6c46b2f0a
Brigitte Hofmeister <bhofmei@gmail.com>
2017-08-15 15:44:38 -0400

    register track types

--- js/main.js
+++ js/main.js
@@ -13,7 +13,14 @@ define([
         // do anything you need to initialize your plugin here
         console.log('StrandedPlotPlugin starting');
         this.config.version = "1.1.2";
-
+        browser.registerTrackType({
+          label: 'StrandedSVGPlot',
+          type: 'StrandedPlotPlugin/View/Track/Wiggle/StrandedSVGPlot'
+        });
+        browser.registerTrackType({
+          label: 'StrandedXYPlot',
+          type: 'StrandedPlotPlugin/View/Track/Wiggle/StrandedXYPlot'
+        });
       }
     });
   });
